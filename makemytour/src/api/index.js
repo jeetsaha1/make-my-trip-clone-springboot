da295b86bd31ed1a2505d1b70c6becacd5f21a0e
Jeet Saha <jeet2022saha@gmail.com>
2026-06-07 10:31:40 +0530

    Prepare for Render deployment

--- makemytour/src/api/index.js
+++ makemytour/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
+const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "https://make-my-trip-clone-springboot-2tzx.onrender.com";
 
 export const login = async (email, password) => {
   try {
